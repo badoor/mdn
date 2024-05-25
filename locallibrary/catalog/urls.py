b5efee5f99349c9b01152d56cbe351e95f3153aa
badoor <ba7941@gmail.com>
2024-05-25 19:12:14 +0300

    new22

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     re_path(r'^books/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -7,16 +7,21 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     num_genres = Genre.objects.all().count()
 
     return render(request, 'index.html', context={
-        'num_books':num_books, 'num_instances':num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genres': num_genres
+        'num_books':num_books, 'num_instances':num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genres': num_genres, 'num_visits': num_visits
     })
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 1
+    paginate_by = 10
 
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+def renew_book_librarian(self):
+    pass
